9cf10a6afb507bc619369a579652619f627a7d59
Johnathan Thao <johnathanthao21@gmail.com>
2022-07-09 21:17:05 -0600

    Done

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Add your code here
+function submitData(nameInput,emailInput){
+    const info = {
+        name: nameInput,
+        email: emailInput,
+    }
+    return fetch("http://localhost:3000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify(info),
+    })
+    .then(response => response.json())
+      .then(myFunction => {
+          console.log(myFunction)
+          const p = document.createElement("p")
+          p.textContent = myFunction.id
+          p.innerText =`{id: ${myFunction.id}, name: ${myFunction.name}, email: ${myFunction.email}}`
+          document.body.appendChild(p)
+    })
+    .catch((error) =>{
+        let message = error.message
+        // const p = document.createElement("p")
+        // p.innerHTML = message;
+        document.body.append(message)
+      });
+    
+    }
+    submitData("John", "johnathanthao21@gmail.com")
